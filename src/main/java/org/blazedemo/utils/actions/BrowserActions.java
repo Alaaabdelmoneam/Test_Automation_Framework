c34d61259590db323e4541d4c826d08aaf14ab17
Alaaabdelmoneam <alol1922002@gmail.com>
2026-06-12 12:44:20 +0300

    Added Basic BrowserActions for readability

---
+++ src/main/java/org/blazedemo/utils/actions/BrowserActions.java
@@ -0,0 +1,9 @@
+package org.blazedemo.utils.actions;
+
+import org.blazedemo.drivers.DriverManager;
+
+public class BrowserActions {
+    public static void navigateTo(String URL){
+        DriverManager.getDriver().get(URL);
+    }
+}
